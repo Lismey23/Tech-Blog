d60e7efb19d905589bbe6aa8254cf4fca363cf60
Lismey Plasencia <lismeyplasencia@gmail.com>
2021-04-13 17:01:58 -0400

    added routes

--- controllers/api/index.js
+++ controllers/api/index.js
@@ -1,1 +1,11 @@
-// Add the routes file to the api folder
+const router = require('express').Router();
+const userRoutes = require('./userRoutes');
+const postRoutes = require('./postsRoutes');
+const commentRoutes = require('./commentRoutes');
+
+router.use('/users', userRoutes);
+router.use('/posts', postRoutes);
+router.use('/comments', commentRoutes);
+
+module.export = router;
+
